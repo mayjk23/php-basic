1317dc2159d30d6be860d367f3d95e35979c7223
mayjk23 <juthathip_kongpun@outlook.com>
2020-05-04 21:42:02 +0700

    chapter 9, 10 array

---
+++ 10_array2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP Basic</title>
+</head>
+
+<body>
+
+    <?php
+
+    $name = array("THAI", "KOREA", "CHINA");
+
+    $country=array
+    echo "<br>";
+
+
+
+
+
+    ?>
+
+
+</body>
+
+</html>
--- 9_array.php
+++ 9_array.php
@@ -8,7 +8,18 @@
 <body>
 
     <?php
-    //ตัวแปรหนึ่งตัว ที่เก็บค่าหลายๆ ตัว เป็นกลุ่ม
+
+    $name = array("Red", "Green", "Blue");
+
+    echo $name[1];   //แสดงข้อมูลเฉพาะตำแหน่งที่ 1
+    echo "<br>";
+    print_r($name);  //แสดงข้อมูลใน array ทั้งหมด
+    echo "<br>";
+
+    //นำข้อมูลตัวแปร array ไปใส่เป็นตัวแปรใช้ foreach วนแสดงผล
+    foreach ($name as $nameshowall) {
+        echo "$nameshowall <br>";
+    }
 
 
 
